2591a833e66b12596fa803f2f241166bf6a3f6c6
Afa Ferz <afa00.papa@gmail.com>
2024-01-27 19:19:43 -0300

    feat: add icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
         head: {
             charset: 'utf-8',
             viewport: 'width=device-width, initial-scale=1',
-            title: 'Font Aldrigues'
+            title: 'Font Aldrigues',
+            link: [{ rel: 'icon', type: 'image/png', href: '/favicon.ico' }],
         }
     },
     devtools: { enabled: true },
